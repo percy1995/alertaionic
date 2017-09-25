d68cffadabcba2dd20b8fabe241cd6db61b6b9ec
unknown <zavaleta_percy@hotmail.com>
2017-09-25 15:08:42 -0500

    alertaionic

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController,AlertController } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +7,17 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController,public alertCtrl:AlertController) {
+  }
+    doAlert(){
+      let alert=this.alertCtrl.create({
+        title:'Nueva amigo',
+        message:'Hola esto e suna alerta',
+        buttons:  ['ok']
 
+      });
+   
+alert.present();
   }
 
 }
